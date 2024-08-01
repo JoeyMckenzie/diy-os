ad9298a99450ceae1fb6f3f6ca827e405dfa4102
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-08-01 09:14:32 -0700

    chore: wip

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -1,10 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-class DocumentController extends Controller
+final class DocumentController extends Controller
 {
     //
 }
--- resources/js/pages/landing/partials/Pricing.tsx
+++ resources/js/pages/landing/partials/Pricing.tsx
@@ -64,7 +64,7 @@ export function Pricing() {
                                     </span>
                                 </p>
                                 <a
-                                    href="#"
+                                    href={route("welcome")}
                                     className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center font-semibold text-sm text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-indigo-600 focus-visible:outline-offset-2"
                                 >
                                     Sign up
